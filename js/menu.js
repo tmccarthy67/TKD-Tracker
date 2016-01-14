f8a252f820b041955d20a71dfc547bb5dac6e38e
tmccarthy67 <troymccarthy67@gmail.com>
2016-01-13 19:59:22 -0600

    added menu as JS to be editable

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+// build menu
+//Resources
+$("#TKDResources").append('<li class="dropList"><a href="forms.html">Forms</a></li>');
+$("#TKDResources").append('<li class="dropList"><a href="onesteps.html">One Steps</a></li>');
+$("#TKDResources").append('<li class="dropList"><a href="songbongs.html">Song Bongs</a></li>');
+$("#TKDResources").append('<li class="dropList"><a href="belts.html">Belts</a></li>');
+$("#TKDResources").append('<li class="dropList"><a href="breaking.html">Breaking</a></li>');
+$("#TKDResources").append('<li class="dropList"><a href="terms.html">Terms</a></li>');
+//Claas Info
+$("#ClassInfo").append('<li class="dropList"><a href="about.html">About TRCMA</a></li>');
+$("#ClassInfo").append('<li class="dropList"><a href="instructors.html">Instructors</a></li>');
+$("#ClassInfo").append('<li class="dropList"><a href="policy.html">Policy</a></li>');
+$("#ClassInfo").append('<li class="dropList"><a href="rules.html">Rules</a></li>');
+// $("#ClassInfo").append('<li class="dropList"><a href="login.html">Login</a></li>');
+
