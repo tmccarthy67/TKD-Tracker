ad140057f65fad10f03c3bd60f6a973bac31c36b
tmccarthy67 <troymccarthy67@gmail.com>
2016-01-19 14:30:50 -0600

    added pages to main

--- js/menu.js
+++ js/menu.js
@@ -2,14 +2,15 @@
 //Resources
 $("#TKDResources").append('<li class="dropList"><a href="forms.html">Forms</a></li>');
 $("#TKDResources").append('<li class="dropList"><a href="onesteps.html">One Steps</a></li>');
-$("#TKDResources").append('<li class="dropList"><a href="songbongs.html">Song Bongs</a></li>');
+$("#TKDResources").append('<li class="dropList"><a href="sajukongbong.html">Sa Ju Kong Bongs</a></li>');
 $("#TKDResources").append('<li class="dropList"><a href="belts.html">Belts</a></li>');
 $("#TKDResources").append('<li class="dropList"><a href="breaking.html">Breaking</a></li>');
 $("#TKDResources").append('<li class="dropList"><a href="terms.html">Terms</a></li>');
+$("#TKDResources").append('<li class="dropList"><a href="documents.html">Documents</a></li>');
+$("#TKDResources").append('<li class="dropList"><a href="pictures.html">Pictures</a></li>');
+$("#TKDResources").append('<li class="dropList"><a href="videos.html">Videos</a></li>');
 //Claas Info
 $("#ClassInfo").append('<li class="dropList"><a href="about.html">About TRCMA</a></li>');
 $("#ClassInfo").append('<li class="dropList"><a href="instructors.html">Instructors</a></li>');
 $("#ClassInfo").append('<li class="dropList"><a href="policy.html">Policy</a></li>');
 $("#ClassInfo").append('<li class="dropList"><a href="rules.html">Rules</a></li>');
-$("#ClassInfo").append('<li class="dropList"><a href="pictures.html">Pictures</a></li>');
-
